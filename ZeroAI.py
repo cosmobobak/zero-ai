b29b840322de81e28c97ed2a5779e99af22107ba
cosmobobak <56003038+cosmobobak@users.noreply.github.com>
2021-06-22 20:50:04 +0100

    Update ZeroAI.py

--- ZeroAI.py
+++ ZeroAI.py
@@ -3,15 +3,10 @@ from os import getenv
 from dotenv import load_dotenv
 
 import chess.svg
-from Glyph import State
 from chess import Board, Move
-from viridithas_chess.src.Viridithas import Viridithas
 from random import choice, random
 
 COMMAND_CHARACTER = '!'
-
-TICTACTOE_RUNNING = False
-CHESS_RUNNING = False
 
 lads = {"finegold", "gotham", "jamie", "kit", "mike", "cosmo",
         "edward", "marina", "tegan", "elyn", "roman"}
@@ -24,16 +19,6 @@ load_dotenv()
 
 token = getenv('TOKEN')
 
-TTT_GAME = State()
-CHESS_GAME = Board()
-CHESS_ENGINE = Viridithas(
-    human=False, 
-    fen=CHESS_GAME.fen(), 
-    pgn='', 
-    timeLimit=15,
-    book=True,
-    fun=True,  
-    contempt=3000)
 
 client = discord.Client()
 
@@ -94,48 +79,6 @@ async def on_message(msg):
 
 async def send(message, text):
     await message.channel.send(text)
-
-async def viridithas_engine_move(message, message_content):
-    global CHESS_GAME
-    move: Move = CHESS_GAME.parse_san(message_content[1])
-
-    CHESS_GAME.push(move)
-    CHESS_ENGINE.node.push(move)
-
-    await message.channel.send(
-        f"The board after your move:\n```\n{CHESS_GAME.unicode()}\n```")
-
-    if CHESS_GAME.is_game_over():
-        await message.channel.send(CHESS_GAME.result())
-    
-    await message.channel.send("Starting to think!")
-    move = CHESS_ENGINE.engine_move()
-    await message.channel.send("Finished thinking.")
-    CHESS_GAME.push(move)
-
-    await message.channel.send(
-        f"The board after Viri's move:```\n{CHESS_GAME.unicode()}\n```\n{CHESS_ENGINE.last_search()}")
-
-    if CHESS_GAME.is_game_over():
-        await message.channel.send(CHESS_GAME.result())
-
-async def not_programmed(message):
-    return await message.channel.send('I am not programmed to play that game.')
-
-async def run_ttt(message):
-    global TTT_GAME
-    global TICTACTOE_RUNNING
-    await message.channel.send('A nice game of noughts and crosses.')
-    TTT_GAME = State()
-    await message.channel.send(f"```\n{TTT_GAME.__repr__()}\n```")
-    TICTACTOE_RUNNING = True
-
-async def run_chess(message):
-    global CHESS_RUNNING
-    await message.channel.send('A nice game of chess.')
-    CHESSgame = Board()
-    await message.channel.send(f"```\n{CHESSgame.unicode()}\n```\nType \"move [san]\" to make a move.")
-    CHESS_RUNNING = True
 
 def choose_three_pieces():
     return ", ".join([choice(["pawn", "knight", "bishop", "rook", "queen", "king"]) for _ in range(3)])
