661f4afba3954957c6ca823f0ddc1bd234b847f7
cosmobobak <cosmobobak@gmail.com>
2021-07-23 18:53:49 +0100

    refactor out commands

--- ZeroAI.py
+++ ZeroAI.py
@@ -55,50 +55,70 @@ async def on_message(msg):
     head, *tail = cmd
 
     if head == "pieces":
-        pieces = ["pawn", "knight", "bishop", "rook", "queen", "king"]
-        content = ", ".join([choice(pieces) for i in range(3)])
-        await send(msg, content)
+        await pieces(msg)
     if head == "quote":
-        assert len(tail) >= 1
-        name, *_ = tail
-        filename = name + "quotes.txt"
-        with open(filename, 'r') as f:
-            qs = [strip_endline(q) for q in f]
-        await send(msg, f"{name}: \"{choice(qs)}\"")
+        await quote(msg, tail)
     if head == "addquote":
-        assert len(tail) >= 2
-        name, *quotelist = tail
-        assert name in lads or name == "me"
-        if name == "me":
-            name = usermap.get(msg.author, default="NO_PERSON")
-        if name == "NO_PERSON" or name not in lads: return
-        quote = " ".join(quotelist)
-        filename = name + "quotes.txt"
-        with open(filename, 'a') as f:
-            f.write("\n")
-            f.write(quote)
-        await send(msg, f"added quote \"{quote}\" to file {filename}")
+        await addquote(msg, tail)
     if head == "adduser":
-        assert len(tail) == 2
-        name, uid = tail
-        if uid == "me": 
-            save_user(msg.author, name)
-        else: 
-            handle = await map_uid_to_handle(uid)
-            save_user(handle, name)
+        await adduser(msg, tail)
     if head == "quotestats":
-        assert len(tail) >= 1
-        name, *_ = tail
-        filename = name + "quotes.txt"
-        with open(filename, 'r') as f:
-            qs = [strip_endline(q) for q in f]
-        count = len(qs)
-        avglen = int(sum(map(len, qs)) / count + 0.5)
-        await send(msg, f"{name} has {count} quotes, with an average quote length of {avglen}.")
+        await quotestats(msg, tail)
     if head == "ballsdeep":
-        await send(msg, "[SUCCESSFULLY HACKED FBI - BLOWING UP COMPUTER]")
-        for i in range(5, 0, -1):
-            await send(msg, f"{i}")
+        await ballsdeep(msg)
+
+async def pieces(msg):
+    pieces = ["pawn", "knight", "bishop", "rook", "queen", "king"]
+    content = ", ".join([choice(pieces) for i in range(3)])
+    await send(msg, content)
+
+async def quote(msg, tail):
+    assert len(tail) >= 1
+    name, *_ = tail
+    filename = name + "quotes.txt"
+    with open(filename, 'r') as f:
+        qs = [strip_endline(q) for q in f]
+    await send(msg, f"{name}: \"{choice(qs)}\"")
+
+async def addquote(msg, tail):
+    assert len(tail) >= 2
+    name, *quotelist = tail
+
+    assert name in lads or name == "me"
+
+    if name == "me":
+        name = usermap.get(msg.author, default="NO_PERSON")
+    if name == "NO_PERSON" or name not in lads: return
+    quote = " ".join(quotelist)
+    filename = name + "quotes.txt"
+    with open(filename, 'a') as f:
+        f.write("\n")
+        f.write(quote)
+    await send(msg, f"added quote \"{quote}\" to file {filename}")
+
+async def adduser(msg, tail):
+    assert len(tail) == 2
+    name, uid = tail
+    if uid == "me": 
+        save_user(msg.author, name)
+    else: 
+        handle = await map_uid_to_handle(uid)
+        save_user(handle, name)
+
+async def quotestats(msg, tail):
+    assert len(tail) >= 1
+    name, *_ = tail
+    filename = name + "quotes.txt"
+    with open(filename, 'r') as f:
+        qs = [strip_endline(q) for q in f]
+    count = len(qs)
+    avglen = int(sum(map(len, qs)) / count + 0.5)
+    await send(msg, f"{name} has {count} quotes, with an average quote length of {avglen}.")
+
+async def ballsdeep(msg):
+    await send(msg, "[SUCCESSFULLY HACKED FBI - BLOWING UP COMPUTER]")
+    for i in range(5, 0, -1):
+        await send(msg, f"{i}")
 
 async def send(message, text):
     await message.channel.send(text)
